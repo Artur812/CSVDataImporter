91ccd898e1f34180b373945919e2fba1f87e7591
Artur <arturaitmuratov@gmail.com>
2023-10-03 09:41:09 +0500

    Add project files.

---
+++ CSVDataImporter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSVDataImporter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
